8a3655a656e8a20755bfd7d26ef0b4541daa4ca4
Li Li <LiLi@ACYCAPITAL.local>
2018-01-22 15:20:20 +1100

    mass lead

--- src/app/view/leads/leads-new-view/leads-new-view.component.ts
+++ src/app/view/leads/leads-new-view/leads-new-view.component.ts
@@ -6,18 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leads-new-view.component.css']
 })
 export class LeadsNewViewComponent implements OnInit {
-  filters = [
-    { field: '', condition: '', value: '' }
-  ];
-  newFilter = { field: '', condition: '', value: '' };
-  fields = [
-    { id: '1', text: 'Name', value: '' },
-    { id: '2', text: 'Salutation', option: ['Mr.', 'Ms.', 'Mrs', 'Dr.', 'Prof.'] },
-    { id: '3', text: 'Last Name', value: '' },
-    { id: '4', text: 'First Name', value: '' },
-    { id: '5', text: 'Lead Recoard Type', value: '' }
-  ];
+  viewName = '';
   conditions = ['none', 'equals', 'not equal to', 'starts with', 'contains', 'does not contain'];
+  allFields = [
+    { id: '1', text: 'Name', type: 'text' },
+    {
+      id: '2',
+      text: 'Salutation',
+      type: 'pickList',
+      option: ['Mr.', 'Ms.', 'Mrs', 'Dr.', 'Prof.']
+
+    },
+    { id: '3', text: 'Last Name', type: 'text' },
+    { id: '4', text: 'First Name', type: 'text' },
+    { id: '5', text: 'Lead Recoard Type', type: 'text' },
+    {
+      id: '6',
+      text: 'Country',
+      type: 'pickList',
+      option: ['China', 'Australia', 'America']
+    }
+  ];
+
+  availableFields = [
+    { id: '1', text: 'Name' },
+    {
+      id: '2',
+      text: 'Salutation',
+      option: ['Mr.', 'Ms.', 'Mrs', 'Dr.', 'Prof.']
+
+    },
+    { id: '3', text: 'Last Name' },
+    { id: '4', text: 'First Name' },
+    { id: '5', text: 'Lead Recoard Type' }
+  ];
+
+  filters = [{ index: '0', field: '2', condition: 'equals', value: 'Mr.,Ms.,aaaaa' }];
+  selectedFields = [];
+
+  // Function Initial Variable
+  sidebarShow = false;
+  pickList = [];
+  selectedValue = [];
+  pickHeader = '';
+  pickListIndex;
 
   activeOption = -1;
 
@@ -25,28 +57,112 @@ export class LeadsNewViewComponent implements OnInit {
   addJson = null;
   addAfter = -1;
 
-  availableFields = [
-    { id: '1', text: 'Name', value: '' },
-    { id: '2', text: 'Salutation', value: '' },
-    { id: '3', text: 'Last Name', value: '' },
-    { id: '4', text: 'First Name', value: '' },
-    { id: '5', text: 'Lead Recoard Type', value: '' }
-  ];
-  selectedFields = [];
-
-  addFilter() {
-    this.filters.push(this.newFilter);
-  }
-
   findObjectByKey(array, key, value) {
     for (let i = 0; i < array.length; i++) {
       if (array[i][key] == value) {
-        const obj = { 'no': i, 'json': array[i] }
+        const obj = { 'no': i, 'json': array[i] };
         return obj;
       }
     }
     return null;
   }
+
+  // Filter Option
+  addFilter() {
+    const newFilter = { index: '', field: '', condition: '', value: '' };
+    newFilter.index = this.filters.length.toString();
+    this.filters.push(newFilter);
+  }
+
+  deleteFilter(i) {
+    this.filters.splice(i, 1);
+  }
+
+  valueType(id) {
+    for (let i = 0; i < this.allFields.length; i++) {
+      if (this.allFields[i]['id'] === id) {
+        const type = this.allFields[i].type;
+        return type;
+      }
+    }
+    return 'text';
+  }
+
+  clearFilterValue(no) {
+    this.filters.forEach(element => {
+      if (element.index === no) {
+        element.value = '';
+      }
+    });
+  }
+
+  // Pick List Option
+  changeSelected(filter) {
+    if (filter.value) {
+      this.selectedValue = filter.value.split(',');
+    } else {
+      this.selectedValue = [];
+    }
+  }
+  openPickList(no, id, value) {
+    this.pickList = [];
+    this.pickListIndex = no;
+    if (value) {
+      this.selectedValue = value.split(',');
+    } else {
+      this.selectedValue = [];
+    }
+    const obj = this.findObjectByKey(this.allFields, 'id', id);
+    const field = obj.json;
+    this.pickHeader = field.text;
+    const allList = field.option;
+    // checked the exist value
+    allList.forEach(element => {
+      this.pickList.push({ option: element, selected: false });
+      // const exist = this.selectedValue.indexOf(element);
+      // if (exist === -1) {
+      //   this.pickList.push({ option: element, selected: false });
+      // } else {
+      //   this.pickList.push({ option: element, selected: true });
+      // }
+    });
+  }
+  showPickList() {
+    this.sidebarShow = true;
+  }
+  submitPickList() {
+    // check the selected List
+    const selectList = [];
+    for (let i = 0; i < this.pickList.length; i++) {
+      if (this.pickList[i].selected) {
+        selectList.push(this.pickList[i].option);
+      }
+    }
+
+    // compare select list and selected value, add new option to selected value
+    selectList.forEach(element => {
+      const exist = this.selectedValue.indexOf(element);
+      if (exist === -1) {
+        this.selectedValue.push(element);
+      }
+    });
+
+    const selectString = this.selectedValue.toString();
+    console.log(selectString);
+    this.filters.forEach(element => {
+      if (element.index === this.pickListIndex) {
+        element.value = selectString;
+      }
+    });
+    this.sidebarShow = false;
+  }
+  closePickList() {
+    this.sidebarShow = false;
+    this.pickHeader = '';
+    this.pickList = [];
+  }
+
+  // Selete Fields Section
 
   addField() {
     const obj = this.findObjectByKey(this.availableFields, 'id', this.activeOption);
@@ -54,7 +170,6 @@ export class LeadsNewViewComponent implements OnInit {
       this.availableFields.splice(obj.no, 1);
       this.selectedFields.push(obj.json);
     }
-
   }
 
   removeField() {
--- src/app/view/leads/leads/leads.component.ts
+++ src/app/view/leads/leads/leads.component.ts
@@ -7,21 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeadsComponent implements OnInit {
   paginationTextShow = true;
-  theaders = [
-    { 'text': 'Name', 'jsonKey': 'name' },
-    { 'text': 'State/Province', 'jsonKey': 'state' },
-    { 'text': 'Country', 'jsonKey': 'country' },
-    { 'text': 'Phone', 'jsonKey': 'phone' },
-    { 'text': 'Email', 'jsonKey': 'email' },
-    { 'text': 'Lead Status', 'jsonKey': 'leadStatus' },
-    { 'text': 'Created Date', 'jsonKey': 'createdDate' },
-    { 'text': 'Account Manager', 'jsonKey': 'accountManager' },
-    { 'text': 'Lead Type', 'jsonKey': 'leadType' },
-    { 'text': 'Agent', 'jsonKey': 'agent' },
-    { 'text': 'Created By', 'jsonKey': 'createdBy' },
-    { 'text': 'Live Suffix', 'jsonKey': 'liveSuffix' },
-    { 'text': 'Demo Suffix', 'jsonKey': 'demoSuffix' },
-    { 'text': 'Gauge', 'jsonKey': 'gauge' }
+  popupShow = false;
+
+  fields = [
+    { id: '1', 'type': 'input', 'text': 'Name', 'jsonKey': 'name' },
+    { id: '2', 'type': 'input', 'text': 'State/Province', 'jsonKey': 'state' },
+    { id: '3', 'type': 'option', 'text': 'Country', 'jsonKey': 'country', option: ['China', 'Australia', 'America'] },
+    { id: '4', 'type': 'input', 'text': 'Phone', 'jsonKey': 'phone' },
+    { id: '5', 'type': 'input', 'text': 'Email', 'jsonKey': 'email' },
+    { id: '6', 'type': 'input', 'text': 'Lead Status', 'jsonKey': 'leadStatus' },
+    { id: '7', 'type': 'input', 'text': 'Created Date', 'jsonKey': 'createdDate' },
+    { id: '8', 'type': 'option', 'text': 'Account Manager', 'jsonKey': 'accountManager' },
+    { id: '9', 'type': 'input', 'text': 'Lead Type', 'jsonKey': 'leadType' },
+    { id: '10', 'type': 'input', 'text': 'Agent', 'jsonKey': 'agent' },
+    { id: '11', 'type': 'input', 'text': 'Created By', 'jsonKey': 'createdBy' },
+    { id: '12', 'type': 'input', 'text': 'Live Suffix', 'jsonKey': 'liveSuffix' },
+    { id: '13', 'type': 'input', 'text': 'Demo Suffix', 'jsonKey': 'demoSuffix' },
+    { id: '14', 'type': 'input', 'text': 'Gauge', 'jsonKey': 'gauge' }
   ];
   Json = [
     {
@@ -537,6 +539,8 @@ export class LeadsComponent implements OnInit {
       'gauge': 'Medium'
     }
   ];
+
+  fieldIndex;
 
   constructor() { }
 
