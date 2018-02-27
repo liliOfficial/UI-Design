65b6f9af72517d0e797716ab35c35b90b3f6c401
liliOfficial <lilipromail@gmail.com>
2018-02-27 14:52:08 +1100

    commit

--- src/app/view/setup/leads/leads-page-layout/leads-page-layout.component.ts
+++ src/app/view/setup/leads/leads-page-layout/leads-page-layout.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeadsPageLayoutComponent implements OnInit {
   newPopup = false;
+  edit = false;
   layouts = [
     {
       name: 'Company Administrator',
